3011c6a6dc5671d3fe791ff98c816465e5b91517
Lucacks <alveslucaso094@gmail.com>
2024-04-11 21:33:02 -0300

    feat: add win and tie functions

--- games/fighting-game/index.js
+++ games/fighting-game/index.js
@@ -111,6 +111,38 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
     )
 }
 
+function determineWinner({ player, enemy, timerId }) {
+    clearTimeout(timerId)
+
+    document.querySelector('.tie').style.display = 'flex'
+
+    if (player.health === enemy.health) {
+        document.querySelector('.tie').innerHTML = 'Empate!'
+    }
+    if (player.health > enemy.health) {
+        document.querySelector('.tie').innerHTML = 'Vitória do Player!'
+    }
+    if (player.health < enemy.health) {
+        document.querySelector('.tie').innerHTML = 'Vitória do Inimigo!'
+    }
+}
+
+let timer = 60
+let timerId
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000)
+        timer--
+        document.querySelector('.timer').innerHTML = timer
+    }
+
+    if (timer === 0) {
+        determineWinner({ player, enemy })
+    }
+}
+
+decreaseTimer()
+
 function animate() {
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
@@ -152,6 +184,12 @@ function animate() {
         enemy.isAttacking = false
         player.health -= 20
         document.querySelector('.player-hp').style.width = player.health + '%'
+    }
+
+    // End game
+
+    if (enemy.health <= 0 || player.health <= 0) {
+        determineWinner({ player, enemy, timerId })
     }
 }
 
